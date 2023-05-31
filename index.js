a4f5e51177b943128c1308c0516bea8c617af4b6
VanQuoc203 <vanquoc203nt@gmail.com>
2023-05-31 22:43:32 +0700

    handlebars

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const handlebars = require('express-handlebars').engine;
 const app = express();
 const path = require('path');
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const port = 5000
 //HTTP
 app.use(morgan("combined"));
-app.engine('handlebars',handlebars())
+app.engine('handlebars',handlebars());
 app.set('view engine', 'handlebars');
 
 app.set('views', './views');
