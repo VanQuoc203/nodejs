24ebdd5502b8a72d667128d4c0b00425d28f381c
VanQuoc203 <vanquoc203nt@gmail.com>
2023-05-31 22:42:19 +0700

    handlebars

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const handlebars = require('express-handlebars').engine;
+const app = express();
+const path = require('path');
+const morgan = require("morgan");
+const port = 5000
+//HTTP
+app.use(morgan("combined"));
+app.engine('handlebars',handlebars())
+app.set('view engine', 'handlebars');
 
-app.get('/tin-tuc', (req, res) => {
-  res.send('Hello World!')
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render("home")
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  
 })
